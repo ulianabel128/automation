a5eb2c0d5d62e5fe6baa5407066a2d0e7c843f83
Belkina Ulyana <belkina@hostco.ru>
2021-07-28 09:30:48 +0500

    #66295 Изменения по комментам

--- src/test/java/tests/Superadmin.java
+++ src/test/java/tests/Superadmin.java
@@ -13,40 +13,12 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import utils.*;
 
-
 import java.io.FileInputStream;
 import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class Superadmin extends BaseConfigurationTest{
-
-    WebDriverWait wait;
-    JavascriptExecutor js;
-    Actions action;
-    FileInputStream fis;
-    Properties property;
-
-    @BeforeTest
-    public void beforeTest() throws Exception {
-
-        driver = DriversManager.getDriver();
-
-        //настраиваем ожидание
-        wait = new WebDriverWait(driver,10);
-
-        //настройка для работы с js
-        js =  ((JavascriptExecutor) driver);
-        action = new Actions(driver);
-
-        //property
-        property = new Properties();
-        fis = new FileInputStream("src/main/resources/config.properties");
-        property.load(fis);
-
-        //Открываем браузер
-        driver.get(cfg.getBaseUriProperties());
-    }
 
     @Test
     public void creationNews() {
@@ -55,14 +27,15 @@ public class Superadmin extends BaseConfigurationTest{
         driver.findElement(By.xpath("//a[contains(text(), 'Войти в кабинет')]")).click();
 
         //ввод логина и пароля
-        driver.findElement(By.id("username")).sendKeys("16067057157");
-        driver.findElement(By.id("password")).sendKeys("123");
+        driver.findElement(By.id("username")).sendKeys(property.getProperty("user.login"));
+        driver.findElement(By.id("password")).sendKeys(property.getProperty("user.password"));
 
         //кнопка "Войти"
         driver.findElement(By.id("btn")).click();
 
         //ЛК
-        driver.manage().timeouts().pageLoadTimeout(Integer.parseInt(property.getProperty("page.time").trim()), TimeUnit.MILLISECONDS);
+        //driver.manage().timeouts().pageLoadTimeout(Integer.parseInt(property.getProperty("page.time").trim()), TimeUnit.MILLISECONDS);
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//a[contains(text(), 'Колташева Александра ')]")));
         driver.findElement(By.xpath("//a[contains(text(), 'Колташева Александра ')]")).click();
         driver.manage().timeouts().implicitlyWait(Integer.parseInt(property.getProperty("element.time").trim()), TimeUnit.MILLISECONDS);
         driver.findElement(By.xpath("//span[contains(text(), 'Личный кабинет')]")).click();
@@ -86,9 +59,9 @@ public class Superadmin extends BaseConfigurationTest{
         wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//h2[contains(text(), 'Новости')]")));
         driver.findElement(By.xpath("//input[@class='ui-inputtext ui-corner-all ui-state-default ui-widget ng-untouched ng-pristine ng-invalid']"))
                 .sendKeys("heading1");
-        driver.findElement(By.xpath("//div[@class='ng-tns-c57-18 dropdown ui-dropdown ui-widget ui-state-default ui-corner-all ui-dropdown-clearable']"))
+        driver.findElement(By.xpath("//div[@class='ng-tns-c57-17 dropdown ui-dropdown ui-widget ui-state-default ui-corner-all ui-dropdown-clearable']"))
                 .click();
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='ui-dropdown-items-wrapper ng-tns-c57-18']")));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='ui-dropdown-items-wrapper ng-tns-c57-17']")));
         driver.findElement(By.xpath("//li[starts-with(@aria-label, 'Общее')]"))
                 .click();
 
@@ -108,26 +81,8 @@ public class Superadmin extends BaseConfigurationTest{
             Assert.assertTrue(heading.equals("heading1"));
         }
         catch (Exception e) {
-            System.out.println(e.getMessage());
+            logger.info(e.getMessage());
         }
     }
 
-    @AfterTest
-    public void afterTes() throws Exception{
-        try {
-            //Удаляем новость
-            List<WebElement> deleteButton=driver.findElements(By.xpath("//i[@class='ico icon-close']"));
-            deleteButton.get(0).click();
-            wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[contains(text(), 'Да ')]")));
-            driver.findElement(By.xpath("//button[contains(text(), 'Да ')]"))
-                    .click();
-        }
-        catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-        finally {
-            DriversManager.stopDriver();
-        }
-
-    }
 }
--- src/test/java/utils/BaseConfigurationTest.java
+++ src/test/java/utils/BaseConfigurationTest.java
@@ -1,30 +1,47 @@
 package utils;
 
+import static config.ConfigSingle.cfg;
+
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import java.util.List;
+import java.util.Properties;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.testng.annotations.BeforeTest;
 
 
 public class BaseConfigurationTest {
 
     public static WebDriver driver;
-    private static final Logger logger = LogManager.getLogger();
+    public static final Logger logger = LogManager.getLogger();
+    public static WebDriverWait wait;
+    public static JavascriptExecutor js;
+    public static Actions action;
+    public static FileInputStream fis;
+    public static Properties property;
 
     @BeforeSuite
     public void beforeSuite()
     {
         //настраиваем драйвер
-        System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
-        driver = new ChromeDriver();
+        driver = DriversManager.getDriver();
 
         //логирование
         logger.info("Log4j2ExampleApp started.");
@@ -37,14 +54,52 @@ public class BaseConfigurationTest {
         }
     }
 
+    @BeforeTest
+    public void beforeTest() throws Exception {
+        //настраиваем ожидание
+        wait = new WebDriverWait(driver,10);
+
+        //настройка для работы с js
+        js =  ((JavascriptExecutor) driver);
+        action = new Actions(driver);
+
+        //property
+        property = new Properties();
+        fis = new FileInputStream("src/test/resources/config.properties");
+        property.load(fis);
+
+        //Открываем браузер
+        driver.get(cfg.getBaseUriProperties());
+    }
+
     @BeforeMethod
     public void beforeMethod()
     {
 
     }
 
+
     @AfterMethod
     public void afterMethod(){
+
+    }
+
+    @AfterTest
+    public void afterTest() throws Exception{
+        try {
+            //Удаляем новость
+            List<WebElement> deleteButton=driver.findElements(By.xpath("//i[@class='ico icon-close']"));
+            deleteButton.get(0).click();
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[contains(text(), 'Да ')]")));
+            driver.findElement(By.xpath("//button[contains(text(), 'Да ')]"))
+                .click();
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        finally {
+            DriversManager.stopDriver();
+        }
 
     }
 
